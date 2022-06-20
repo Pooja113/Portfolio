4b1ac4fb30f61a3b158f0db9b5fd26e675bdc9c2
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-06-20 19:15:52 +0530

    huddle

--- frontend/src/Components/Portfolio/Portfolio.jsx
+++ frontend/src/Components/Portfolio/Portfolio.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Portfolio.css";
 import Sharkcart from "../../img/sharkcart.png";
-import { Link } from 'react-router-dom';
+import Huddle from "../../img/huddle.png";
 
 const Portfolio = () => {
   return (
@@ -21,9 +21,9 @@ const Portfolio = () => {
                 </a>
             </div>
             <div className="portfolio__item">
-                <a href="https://sharkcart.herokuapp.com/">
-                <img src={Sharkcart} alt="" />
-                <h2 className="project__heading">SharkCart Ecommerce Project</h2>
+                <a href="https://huddle-chat.herokuapp.com/">
+                <img src={Huddle} alt="" />
+                <h2 className="project__heading">Huddle Chat App</h2>
                 <p className="project__tech">
                        Lorem, ipsum dolor sit amet 
                 </p>

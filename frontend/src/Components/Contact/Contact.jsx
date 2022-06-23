256c8ddbc2ea3e8f2243c6f3511d2171018bdf21
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-06-23 23:58:43 +0530

    Add existing1

--- frontend/src/Components/Contact/Contact.jsx
+++ frontend/src/Components/Contact/Contact.jsx
@@ -1,28 +1,27 @@
 import React, { useContext, useRef, useState } from "react";
 import "./Contact.css";
-import emailjs from "@emailjs/browser";
+//import emailjs from "@emailjs/browser";
 const Contact = () => {
   const form = useRef();
   const [done, setDone] = useState(false)
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs
-      .sendForm(
-        "service_2mu5xtl",
-        "template_m5udu2c",
-        form.current,
-        "VLwg1ltOWvnCYAiK_"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-          setDone(true);
-          form.reset();
-        },
-        (error) => {
-          console.log(error.text);
-        }
+//  .sendForm(
+//         "service_2mu5xtl",
+//         "template_m5udu2c",
+//         form.current,
+//         "VLwg1ltOWvnCYAiK_"
+//       )
+//       .then(
+//         (result) => {
+//           console.log(result.text);
+//           setDone(true);
+//           form.reset();
+//         },
+//         (error) => {
+//           console.log(error.text);
+//         }
       );
   };
 
